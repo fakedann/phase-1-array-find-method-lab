70564ccb223c4674415e9a733fc4ea1f2ff7f609
Daniel Escalona <daniel07escalona@gmail.com>
2022-01-17 15:05:13 -0500

    commit tree a6890d5e9f146ef67dc9d1a7fc4e5e8152ad728f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(arr){
+    
+    let isWin = arr.find( element => element['result'] === 'W');
+    if (isWin === undefined){
+        return isWin;
+    }
+    return isWin['year'];
+}
+
+
